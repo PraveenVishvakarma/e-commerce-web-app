d30af7bb6a1ae60c843130b26d9068b3efe8ee45
Praveen Vishvakarma <praveen15100@gmail.com>
2024-02-14 19:24:14 +0530

    create discount banner

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -1,15 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function HomeBanner() {
   return (
     <div className='relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8'>
         <div className='mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly  '>
-            <div>
-                <h1>Summer Sale</h1>
-                <p>Enjoy Discounts on Selected items</p>
-                <p>Get 50% OFF</p>
+            <div className='mb-8 md:mb-0 text-center'>
+                <h1 className='text-4xl font-bold md:text-5xl text-white mb-4'>Summer Sale</h1>
+                <p className='text-lg text-white md:text-xl mb-2'>Enjoy Discounts on Selected items</p>
+                <p className='text-2xl md:text-5xl font-bold text-yellow-400'>Get 50% OFF</p>
             </div>
-            <div> Hii sekomon bhai</div>
+            <div
+            className='w-1/3 relative aspect-video'>
+                <Image
+                src="/banner-image.png"
+                fill
+                alt='Banner Image'
+                className='object-contain'
+                />
+                
+            </div>
         </div>
     </div>
   )
