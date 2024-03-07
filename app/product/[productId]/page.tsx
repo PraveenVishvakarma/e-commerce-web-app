2b3d11a8db5419ae3d35cdd4cbc639c7ad86edf1
Praveen Vishvakarma <praveen15100@gmail.com>
2024-03-08 01:57:40 +0530

    create rate product UI

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -4,22 +4,27 @@ import ProductDetail from './ProductDetail';
 import ListRating from './ListRating';
 import { products } from '@/utils/products';
 import getProducts from '@/actions/getProducts';
+import AddRating from './AddRating';
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import getProductById from '@/actions/getProductById';
+import NullData from '@/app/components/NullData';
 
 interface IParam{
     productId?:string;
 }
 
 export default async function productPage({params}:{params:IParam}) {
-    const products=await getProducts({category:null});
-    const product=products.find((product)=>{
-        return product.id===params.productId;
-    })
+    const product=await getProductById(params);
+    if(!product){
+        return <NullData title='Product does not exist ' />
+    }
+    const currentUser=await getCurrentUser();
     return (
     <div className='p-8'>
         <Container>
             <ProductDetail product={product} />
             <div className='flex flex-col mt-20 gap-4'>
-                <div>Product rating</div>
+                <AddRating product={product} user={currentUser} />
                 <ListRating product={product} />
             </div>
         </Container>
